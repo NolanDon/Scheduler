f31d4945482f0244f6cc658133de575dcbb2eb05
nolan <nolan796@live.ca>
2019-12-14 16:25:26 -0700

    Add asynchronous action when rendering application to properly test render in storybook

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -7,7 +7,7 @@ import React from "react";
   We import our helper functions from the react-testing-library
   The render function allows us to render Components
 */
-import { render } from "@testing-library/react";
+import { render, act } from "@testing-library/react";
 
 /*
   We import the component that we are testing
@@ -19,8 +19,10 @@ import Appointment from "components/Appointment/index"
   A test that renders a React Component
 */
 
-it("renders without crashing", () => {
+it("renders without crashing", async () => {
+  await act(async () => {
   render(<Application />);
+  });
 });
 describe("Appointment", () => {
   it("renders without crashing", () => {
